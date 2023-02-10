08664098bb08e92afc2a02a301408c2291e92c6b
bhuvaneswari42 <124863378+bhuvaneswari42@users.noreply.github.com>
2023-02-10 21:14:51 +0530

    Add files via upload

---
+++ ARMSTRON.C
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,c,d=0,e;
+ clrscr();
+ printf("enter the number :");
+ scanf("%d",&a);
+ e=a;
+ while(a>0)
+ {
+  b=a%10;
+  c=b*b*b;
+  a=a/10;
+  d=c+d;
+  }
+   if(e==d)
+   printf("%d is an armstrong number.",e);
+   else
+   printf("%d is nat an armstrong number.",e);
+   getch();
+   }
