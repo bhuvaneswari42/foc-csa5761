e8f9825602abd83317805355015edfa68da27af7
bhuvaneswari42 <124863378+bhuvaneswari42@users.noreply.github.com>
2023-02-13 17:15:06 +0530

    Add files via upload

---
+++ ADDSUBMU.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b;
+ clrscr();
+ printf("enter the values :");
+ scanf("%d%d",&a,&b);
+ printf("%d:addition \n" , a+b);
+ printf("%d:subtraction \n",a-b);
+ printf("%d:multiplication \n",a*b);
+ printf("%d:division");
+ getch();
+ }
